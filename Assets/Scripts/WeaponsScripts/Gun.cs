6ad9c89d3486f2170a2042e44b6f42ff4c90d05c
Dmitry <ADimaso007@gmail.com>
2024-06-28 14:37:29 +0300

    100% map + prepare to upgrade guns

---
+++ Assets/Scripts/WeaponsScripts/Gun.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGunShot
+{
+    void shot();
+}
+
+public interface IGunHit
+{
+    void hit();
+}
+
+public interface IGunReload
+{
+    void reload();
+}
+
